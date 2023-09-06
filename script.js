f2166c6a67319f399e683fc1525796515a288655
PriyadarsiniPS <priyapremlal2015@gmail.com>
2023-09-06 11:39:30 -0700

    Changes

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-const form =document.getElementsByTagName('form')[0];
-function validate() {
-    var email = document.getElementById("email")
-    var password = document.getElementById("pwd")
-    var regexp = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+)\.([a-z]{2,8})(.[a-z]{2,8})?$/
-    var regexp2 =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
-    if(email.value.trim() ==="" || password.value.trim() ===""){
-        alert("Empty fields");
-        return false;
-}
-else if(!regexp.test(email.value)) {
-    alert("Invalid Email ");
-    return false;
-}
-else if(!regexp2.test(password.value)) {
-    alert("Invalid Password");
-    return false;
-}
-else {
-    return true;
-}
-}
+function validateForm() {
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('pwd');
+    const emailError = document.getElementById('emailError');
 
-form.addEventListener('submit' , function (event) {
-    if(!validate()) {
-        event.preventDefault();
+    // Email validation (checking for '@' symbol)
+    const emailPattern = /\S+@\S+\.\S+/;
+    if (!emailPattern.test(emailInput.value)) {
+        emailError.textContent = 'Email is not valid';
+        emailError.style.color = 'red';
+        emailInput.focus();
+        return false;
+    } else {
+        emailError.textContent = ''; // Clear the error message
     }
-});
+
+    // Password validation (at least one uppercase, one lowercase, one number, and one symbol)
+    const passwordPattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (!passwordPattern.test(passwordInput.value)) {
+        alert('Password should contain at least one uppercase, one lowercase, one number, and one special symbol. Minimum length is 8 characters.');
+        passwordInput.focus();
+        return false;
+    }
+
+    return true; // Form is valid
+}
